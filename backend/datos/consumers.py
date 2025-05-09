156604a7beba776e9e0cb6ac6112b62a8440ab09
Saul203230 <mtzsaul2019@gmail.com>
2025-05-09 15:16:38 -0600

    Se agrego un log o un print para comprobar si el mensaje habia sido recibido en el servidor del websocket

--- backend/datos/consumers.py
+++ backend/datos/consumers.py
@@ -1,15 +1,18 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+# consumers.py
 class MensajeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.channel_layer.group_add("mensajes", self.channel_name)
+        await self.channel_layer.group_add('mensajes', self.channel_name)
         await self.accept()
+        print("Cliente WebSocket conectado y unido al grupo 'mensajes'.")
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("mensajes", self.channel_name)
+        await self.channel_layer.group_discard('mensajes', self.channel_name)
 
     async def nuevo_mensaje(self, event):
-        await self.send(text_data=json.dumps({
-            'message': event['message']
-        }))
+        mensaje = event['message']
+        print(f"Recibido en consumidor WebSocket: {mensaje}")  # 👈 Este print es clave
+        await self.send(text_data=json.dumps({'mensaje': mensaje}))
+
