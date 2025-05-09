71c2fb56f52433d64987a88153cb0e5014d34e60
Saul203230 <mtzsaul2019@gmail.com>
2025-05-09 15:13:30 -0600

    Se corrigio el nombre incorrecto de una variable

--- backend/dashboard/asgi.py
+++ backend/dashboard/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import datos.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
