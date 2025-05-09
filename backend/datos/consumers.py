6bf2d3dea13dd6ee8f52b051947a9a3963ff3da5
Saul203230 <mtzsaul2019@gmail.com>
2025-05-09 13:45:21 -0600

    Creacion de consumers.py para leer y obtener mensajes

---
+++ backend/datos/consumers.py
@@ -0,0 +1,15 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class MensajeConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add("mensajes", self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard("mensajes", self.channel_name)
+
+    async def nuevo_mensaje(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message']
+        }))
