eca7583799330165c5476d7d940df9a5e078b8aa
Saul203230 <mtzsaul2019@gmail.com>
2025-05-09 15:15:55 -0600

    Se corrigio la funcion ready que estaba fuera de la configuracion de la app

--- backend/datos/apps.py
+++ backend/datos/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class DatosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'datos'
-
-def ready(self):
-    import datos.signals
+    
+    def ready(self):
+        import datos.signals
